7bbccb3465cda21cc11a5f6d35bb32a91c7853a2
AbishaRengitham <abisharengitham@gmail.com>
2024-05-15 22:31:54 +0530

    task 4

---
+++ index.js
@@ -0,0 +1,86 @@
+// {
+//     "basics": {
+//       "name": "ABISHA",
+//       "label": "TASK 4",
+//       "picture": "",
+//       "email": "abisharengitham@gmail.com",
+//       "phone": "7708878502",
+//       "summary": "i love pet animals",
+//       "location": {
+//         "address":"10/29, west street, chettikulam",
+//         "postalCode": "627120",
+//         "city": "tirunelveli",
+//         "countryCode": "+91",
+//         "region": "INDIA"
+//       }
+//     },
+    
+//     "education": [{
+//       "institution": "Anna University",
+//       "area": "EEE",
+//       "studyType": "Bachelor
+//     }]
+// }
+    
+
+// JSON iterate over all for loops (for, for in, for of, for Each)
+
+// for loop
+
+// var numbers = [ 10, 20, 30, 40, 50] 
+// for (var i=0; i < numbers.length; i++) {
+//    console.log(numbers[i])
+// }
+
+//  “While” Loop
+
+//  var infiniteValue = true;
+// while (infiniteValue) {
+//    console.log("Infinite")
+// }
+// ‍
+
+// for in loop 
+
+// const obj = {
+//     "name": "John",
+//     "age": 30,
+//     "city": "New York"
+//   };
+  
+//   for (let key in obj) {
+//     console.log(key + ": " + obj[key]);
+//   }
+
+// For...of loop (for arrays):
+
+// const arr = ["apple", "banana", "orange"];
+
+// for (let element of arr) {
+//   console.log(element);
+// }
+
+// forEach() method (for arrays):
+
+// const arr = ["apple", "banana", "orange"];
+
+// arr.forEach(function(element) {
+//   console.log(element);
+// });
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
